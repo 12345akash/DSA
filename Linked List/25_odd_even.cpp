d8788e8b1e3511faadc8a49fea5755cd9f885e77
Akash kumar gupta <91329259+12345akash@users.noreply.github.com>
2022-12-06 16:10:21 +0530

    Add files via upload

---
+++ Linked List/25_odd_even.cpp
@@ -0,0 +1,21 @@
+//odd even of linked list
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head==NULL || head->next==NULL)
+         return head;
+      ListNode* temp=head;
+      ListNode* ptr=head->next;
+      ListNode* node=ptr;
+      while(ptr!=NULL && ptr->next!=NULL)
+      {
+          temp->next=ptr->next;
+          ptr->next=temp->next->next;
+          temp->next->next=node;
+          temp=temp->next;
+          ptr=ptr->next;
+      }
+
+          return head;
+    }
+};
